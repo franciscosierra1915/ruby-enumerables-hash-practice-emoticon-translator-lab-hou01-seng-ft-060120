a093557de56d8a57c36d6c58a1232c2750da0dfe
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-18 23:13:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path, eng_emo)
   library.each do |meaning, idioms|
       return idioms[:japanese] if idioms[:english] == eng_emo
   end
-  "Sorry, that"
+  "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, jap_emo)
