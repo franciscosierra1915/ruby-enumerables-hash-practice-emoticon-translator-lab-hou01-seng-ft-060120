d43a61a30abf87c70c375441b3d1fb88ba05811a
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-18 23:01:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, eng_emo)
       return idioms[:japanese] if idioms[:english] == eng_emo
   end
 else 
-  message
+  return message
 end
 
 def get_english_meaning(file_path, jap_emo)
